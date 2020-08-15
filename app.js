1cbefd04b97038a0277aa68d5d83fc502f138e7f
bogdanmatei <matei90bogdan@yahoo.com>
2020-08-15 19:08:12 +0100

    button animation

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const writing = document.querySelectorAll("#main-text path");
+// const writing = document.querySelectorAll("#main-text path");
 
-for (let i = 0; i < writing.length; i++) {
-  console.log(`Letter ${i} is ${writing[i].getTotalLength()}`);
+// for (let i = 0; i < writing.length; i++) {
+//   console.log(`Letter ${i} is ${writing[i].getTotalLength()}`);
+// }
+
+function init() {
+  const slides = document.querySelectorAll(".slide");
+  const pages = document.querySelectorAll(".page");
+
+  slides.forEach((slide, index) => {
+    slide.addEventListener("click", function () {
+      changeDots(this);
+    });
+  });
+
+  function changeDots(dot) {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    dot.classList.add("active");
+  }
 }
+
+init();
